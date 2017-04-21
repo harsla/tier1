d05aff8c9d78534e35e9e2a35d87d21c99624bcc
Scott Harsla <harsla@gmail.com>
2017-04-20 20:29:34 -0700

    css min

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
+var cleanCSS = require('gulp-clean-css');
+var concatCss = require('gulp-concat-css');
 
-gulp.task('default', () =>
+gulp.task('compress-img', () =>
   gulp.src('img/**')
   .pipe(imagemin())
   .pipe(gulp.dest('dist/images'))
 );
+gulp.task('minify-css', function() {
+  return gulp.src(['css/**/*.css'])
+    .pipe(concatCss("bundle.css"))
+    .pipe(cleanCSS({debug: true}, function(details) {
+      console.log(details.name + ': ' + details.stats.originalSize);
+      console.log(details.name + ': ' + details.stats.minifiedSize);
+    }))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('default', ['minify-css', 'compress-img']);
